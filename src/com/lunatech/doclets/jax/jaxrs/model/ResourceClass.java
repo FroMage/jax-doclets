ec645b711ac4b136d5f37dbd16576fabeb3e2e19
stephane.epardaud <stephane.epardaud@b858e934-6b8f-11de-a2f9-0f955267958a>
2009-07-08 08:47:35 +0000

    Added LGPL notices

--- src/com/lunatech/doclets/jax/jaxrs/model/ResourceClass.java
+++ src/com/lunatech/doclets/jax/jaxrs/model/ResourceClass.java
@@ -1,3 +1,21 @@
+/*
+    Copyright 2009 Lunatech Research
+    
+    This file is part of jax-doclets.
+
+    jax-doclets is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jax-doclets is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with jax-doclets.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lunatech.doclets.jax.jaxrs.model;
 
 import java.util.Collection;
@@ -15,47 +33,48 @@ import com.sun.javadoc.MethodDoc;
 
 public class ResourceClass {
 
-	private List<ResourceMethod> methods = new LinkedList<ResourceMethod>();
-	private ClassDoc declaringClass;
-	private AnnotationDesc rootPathAnnotation;
-	private AnnotationDesc rootProducesAnnotation;
-	private AnnotationDesc rootConsumesAnnotation;
+  private List<ResourceMethod> methods = new LinkedList<ResourceMethod>();
 
-	public ResourceClass(ClassDoc resourceClass) {
-		// find the annotated class or interface
-		declaringClass = Utils.findAnnotatedClass(resourceClass, Path.class);
-		rootPathAnnotation = Utils.findAnnotation(declaringClass, Path.class);
-		rootProducesAnnotation = Utils.findAnnotation(declaringClass,
-				Produces.class);
-		rootConsumesAnnotation = Utils.findAnnotation(declaringClass,
-				Consumes.class);
-		for (final MethodDoc method : resourceClass.methods(false)) {
-			MethodDoc declaringMethod = Utils.findAnnotatedMethod(
-					declaringClass, method, Path.class);
-			if (declaringMethod != null) {
-				methods.add(new ResourceMethod(method, declaringMethod, this));
-			}
-		}
+  private ClassDoc declaringClass;
 
-	}
+  private AnnotationDesc rootPathAnnotation;
 
-	public ClassDoc getDeclaringClass() {
-		return declaringClass;
-	}
+  private AnnotationDesc rootProducesAnnotation;
 
-	public AnnotationDesc getRootPathAnnotation() {
-		return rootPathAnnotation;
-	}
+  private AnnotationDesc rootConsumesAnnotation;
 
-	public AnnotationDesc getProducesAnnotation() {
-		return rootProducesAnnotation;
-	}
+  public ResourceClass(ClassDoc resourceClass) {
+    // find the annotated class or interface
+    declaringClass = Utils.findAnnotatedClass(resourceClass, Path.class);
+    rootPathAnnotation = Utils.findAnnotation(declaringClass, Path.class);
+    rootProducesAnnotation = Utils.findAnnotation(declaringClass, Produces.class);
+    rootConsumesAnnotation = Utils.findAnnotation(declaringClass, Consumes.class);
+    for (final MethodDoc method : resourceClass.methods(false)) {
+      MethodDoc declaringMethod = Utils.findAnnotatedMethod(declaringClass, method, Path.class);
+      if (declaringMethod != null) {
+        methods.add(new ResourceMethod(method, declaringMethod, this));
+      }
+    }
 
-	public AnnotationDesc getConsumesAnnotation() {
-		return rootConsumesAnnotation;
-	}
+  }
 
-	public Collection<ResourceMethod> getMethods() {
-		return methods;
-	}
+  public ClassDoc getDeclaringClass() {
+    return declaringClass;
+  }
+
+  public AnnotationDesc getRootPathAnnotation() {
+    return rootPathAnnotation;
+  }
+
+  public AnnotationDesc getProducesAnnotation() {
+    return rootProducesAnnotation;
+  }
+
+  public AnnotationDesc getConsumesAnnotation() {
+    return rootConsumesAnnotation;
+  }
+
+  public Collection<ResourceMethod> getMethods() {
+    return methods;
+  }
 }
--- src/com/lunatech/doclets/jax/jaxrs/model/ResourceMethod.java
+++ src/com/lunatech/doclets/jax/jaxrs/model/ResourceMethod.java
@@ -1,3 +1,21 @@
+/*
+    Copyright 2009 Lunatech Research
+    
+    This file is part of jax-doclets.
+
+    jax-doclets is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jax-doclets is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with jax-doclets.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lunatech.doclets.jax.jaxrs.model;
 
 import java.util.ArrayList;
@@ -29,213 +47,198 @@ import com.sun.javadoc.Tag;
 
 public class ResourceMethod implements Comparable<ResourceMethod> {
 
-	private static final Class<?>[] MethodAnnotations = new Class<?>[] {
-			GET.class, POST.class, PUT.class, HEAD.class, DELETE.class };
+  private static final Class<?>[] MethodAnnotations = new Class<?>[] { GET.class, POST.class, PUT.class, HEAD.class, DELETE.class };
 
-	private MethodDoc declaringMethod;
-	private String path;
-	private ResourceClass resource;
-	private ClassDoc declaringClass;
-	private MethodDoc method;
-	final Map<String, MethodParameter> pathParameters = new HashMap<String, MethodParameter>();
-	final Map<String, MethodParameter> matrixParameters = new HashMap<String, MethodParameter>();
-	final Map<String, MethodParameter> queryParameters = new HashMap<String, MethodParameter>();
-	private List<AnnotationDesc> methods = new LinkedList<AnnotationDesc>();
-	private AnnotationDesc producesAnnotation;
-	private AnnotationDesc consumesAnnotation;
+  private MethodDoc declaringMethod;
 
-	private MethodParameter inputParameter;
+  private String path;
 
-	public ResourceMethod(MethodDoc method, MethodDoc declaringMethod,
-			ResourceClass resource) {
-		this.resource = resource;
-		this.method = method;
-		this.declaringClass = resource.getDeclaringClass();
-		this.declaringMethod = declaringMethod;
-		setupPath();
-		setupParameters();
-		setupMethods();
-		setupMIMEs();
-	}
+  private ResourceClass resource;
 
-	private void setupMIMEs() {
-		producesAnnotation = Utils.findMethodAnnotation(declaringClass, method,
-				Produces.class);
-		consumesAnnotation = Utils.findMethodAnnotation(declaringClass, method,
-				Consumes.class);
-		if (producesAnnotation == null) {
-			producesAnnotation = resource.getProducesAnnotation();
-		}
-		if (consumesAnnotation == null) {
-			consumesAnnotation = resource.getConsumesAnnotation();
-		}
-	}
+  private ClassDoc declaringClass;
 
-	private void setupMethods() {
-		for (Class<?> methodAnnotation : MethodAnnotations) {
-			final AnnotationDesc annotation = Utils.findMethodAnnotation(
-					declaringClass, method, methodAnnotation);
-			if (annotation != null)
-				methods.add(annotation);
-		}
-	}
+  private MethodDoc method;
 
-	private void setupParameters() {
-		int i = -1;
-		for (final Parameter parameter : method.parameters()) {
-			i++;
-			final AnnotationDesc pathParamAnnotation = Utils
-					.findParameterAnnotation(declaringMethod, parameter, i,
-							PathParam.class);
-			if (pathParamAnnotation != null) {
-				String name = (String) Utils
-						.getAnnotationValue(pathParamAnnotation);
-				pathParameters.put(name, new MethodParameter(parameter, i,
-						pathParamAnnotation, MethodParameterType.Path,
-						declaringMethod));
-				continue;
-			}
-			final AnnotationDesc matrixParamAnnotation = Utils
-					.findParameterAnnotation(declaringMethod, parameter, i,
-							MatrixParam.class);
-			if (matrixParamAnnotation != null) {
-				String name = (String) Utils
-						.getAnnotationValue(matrixParamAnnotation);
-				matrixParameters.put(name, new MethodParameter(parameter, i,
-						matrixParamAnnotation, MethodParameterType.Matrix,
-						declaringMethod));
-				continue;
-			}
-			final AnnotationDesc queryParamAnnotation = Utils
-					.findParameterAnnotation(declaringMethod, parameter, i,
-							QueryParam.class);
-			if (queryParamAnnotation != null) {
-				String name = (String) Utils
-						.getAnnotationValue(queryParamAnnotation);
-				queryParameters.put(name, new MethodParameter(parameter, i,
-						queryParamAnnotation, MethodParameterType.Query,
-						declaringMethod));
-				continue;
-			}
-			final AnnotationDesc contextAnnotation = Utils
-					.findParameterAnnotation(declaringMethod, parameter, i,
-							Context.class);
-			if (contextAnnotation == null) {
-				this.inputParameter = new MethodParameter(parameter, i, null,
-						MethodParameterType.Input, declaringMethod);
-			}
-		}
-	}
+  final Map<String, MethodParameter> pathParameters = new HashMap<String, MethodParameter>();
 
-	private void setupPath() {
-		final AnnotationDesc pathAnnotation = Utils.findMethodAnnotation(
-				declaringClass, method, Path.class);
-		final String rootPath = (String) Utils.getAnnotationValue(resource
-				.getRootPathAnnotation());
+  final Map<String, MethodParameter> matrixParameters = new HashMap<String, MethodParameter>();
 
-		if (pathAnnotation != null) {
-			String path = (String) Utils.getAnnotationValue(pathAnnotation);
-			this.path = Utils.appendURLFragments(rootPath, path);
-		} else
-			this.path = rootPath;
-	}
+  final Map<String, MethodParameter> queryParameters = new HashMap<String, MethodParameter>();
 
-	public int compareTo(ResourceMethod other) {
-		return path.compareTo(other.path);
-	}
+  private List<AnnotationDesc> methods = new LinkedList<AnnotationDesc>();
 
-	public String toString() {
-		StringBuffer strbuf = new StringBuffer(path);
-		strbuf.append(" ");
-		for (AnnotationDesc method : methods) {
-			strbuf.append(method.annotationType().name());
-			strbuf.append(" ");
-		}
-		return strbuf.toString();
-	}
+  private AnnotationDesc producesAnnotation;
 
-	public List<String> getMethods() {
-		List<String> httpMethods = new ArrayList<String>(methods.size());
-		for (AnnotationDesc method : methods) {
-			httpMethods.add(method.annotationType().name());
-		}
-		return httpMethods;
-	}
+  private AnnotationDesc consumesAnnotation;
 
-	public List<String> getProduces() {
-		if (producesAnnotation == null)
-			return Collections.emptyList();
-		List<String> producedMIMEs = new ArrayList<String>();
-		for (String mime : Utils.getAnnotationValues(producesAnnotation)) {
-			producedMIMEs.add(mime);
-		}
-		return producedMIMEs;
-	}
+  private MethodParameter inputParameter;
 
-	public List<String> getConsumes() {
-		if (consumesAnnotation == null)
-			return Collections.emptyList();
-		List<String> consumedMIMEs = new ArrayList<String>();
-		for (String mime : Utils.getAnnotationValues(consumesAnnotation)) {
-			consumedMIMEs.add(mime);
-		}
-		return consumedMIMEs;
-	}
+  public ResourceMethod(MethodDoc method, MethodDoc declaringMethod, ResourceClass resource) {
+    this.resource = resource;
+    this.method = method;
+    this.declaringClass = resource.getDeclaringClass();
+    this.declaringMethod = declaringMethod;
+    setupPath();
+    setupParameters();
+    setupMethods();
+    setupMIMEs();
+  }
 
-	public MethodDoc getJavaDoc() {
-		return declaringMethod;
-	}
+  private void setupMIMEs() {
+    producesAnnotation = Utils.findMethodAnnotation(declaringClass, method, Produces.class);
+    consumesAnnotation = Utils.findMethodAnnotation(declaringClass, method, Consumes.class);
+    if (producesAnnotation == null) {
+      producesAnnotation = resource.getProducesAnnotation();
+    }
+    if (consumesAnnotation == null) {
+      consumesAnnotation = resource.getConsumesAnnotation();
+    }
+  }
 
-	public String getDoc() {
-		return declaringMethod.commentText();
-	}
+  private void setupMethods() {
+    for (Class<?> methodAnnotation : MethodAnnotations) {
+      final AnnotationDesc annotation = Utils.findMethodAnnotation(declaringClass, method, methodAnnotation);
+      if (annotation != null)
+        methods.add(annotation);
+    }
+  }
 
-	public Tag[] getDocFirstSentence() {
-		return declaringMethod.firstSentenceTags();
-	}
+  private void setupParameters() {
+    int i = -1;
+    for (final Parameter parameter : method.parameters()) {
+      i++;
+      final AnnotationDesc pathParamAnnotation = Utils.findParameterAnnotation(declaringMethod, parameter, i, PathParam.class);
+      if (pathParamAnnotation != null) {
+        String name = (String) Utils.getAnnotationValue(pathParamAnnotation);
+        pathParameters.put(name, new MethodParameter(parameter, i, pathParamAnnotation, MethodParameterType.Path, declaringMethod));
+        continue;
+      }
+      final AnnotationDesc matrixParamAnnotation = Utils.findParameterAnnotation(declaringMethod, parameter, i, MatrixParam.class);
+      if (matrixParamAnnotation != null) {
+        String name = (String) Utils.getAnnotationValue(matrixParamAnnotation);
+        matrixParameters.put(name, new MethodParameter(parameter, i, matrixParamAnnotation, MethodParameterType.Matrix, declaringMethod));
+        continue;
+      }
+      final AnnotationDesc queryParamAnnotation = Utils.findParameterAnnotation(declaringMethod, parameter, i, QueryParam.class);
+      if (queryParamAnnotation != null) {
+        String name = (String) Utils.getAnnotationValue(queryParamAnnotation);
+        queryParameters.put(name, new MethodParameter(parameter, i, queryParamAnnotation, MethodParameterType.Query, declaringMethod));
+        continue;
+      }
+      final AnnotationDesc contextAnnotation = Utils.findParameterAnnotation(declaringMethod, parameter, i, Context.class);
+      if (contextAnnotation == null) {
+        this.inputParameter = new MethodParameter(parameter, i, null, MethodParameterType.Input, declaringMethod);
+      }
+    }
+  }
 
-	public Map<String, MethodParameter> getQueryParameters() {
-		return queryParameters;
-	}
+  private void setupPath() {
+    final AnnotationDesc pathAnnotation = Utils.findMethodAnnotation(declaringClass, method, Path.class);
+    final String rootPath = (String) Utils.getAnnotationValue(resource.getRootPathAnnotation());
 
-	public Map<String, MethodParameter> getPathParameters() {
-		return pathParameters;
-	}
+    if (pathAnnotation != null) {
+      String path = (String) Utils.getAnnotationValue(pathAnnotation);
+      this.path = Utils.appendURLFragments(rootPath, path);
+    } else
+      this.path = rootPath;
+  }
 
-	public Map<String, MethodParameter> getMatrixParameters() {
-		return matrixParameters;
-	}
+  public int compareTo(ResourceMethod other) {
+    return path.compareTo(other.path);
+  }
 
-	public String getPath() {
-		return path;
-	}
+  public String toString() {
+    StringBuffer strbuf = new StringBuffer(path);
+    strbuf.append(" ");
+    for (AnnotationDesc method : methods) {
+      strbuf.append(method.annotationType().name());
+      strbuf.append(" ");
+    }
+    return strbuf.toString();
+  }
 
-	public MethodParameter getInputParameter() {
-		return inputParameter;
-	}
+  public List<String> getMethods() {
+    List<String> httpMethods = new ArrayList<String>(methods.size());
+    for (AnnotationDesc method : methods) {
+      httpMethods.add(method.annotationType().name());
+    }
+    return httpMethods;
+  }
 
-	public boolean isGET() {
-		for (AnnotationDesc method : methods) {
-			if (method.annotationType().name().equals("GET"))
-				return true;
-		}
-		return false;
-	}
+  public List<String> getProduces() {
+    if (producesAnnotation == null)
+      return Collections.emptyList();
+    List<String> producedMIMEs = new ArrayList<String>();
+    for (String mime : Utils.getAnnotationValues(producesAnnotation)) {
+      producedMIMEs.add(mime);
+    }
+    return producedMIMEs;
+  }
 
-	public String getURL(Resource resource) {
-		StringBuffer strbuf = new StringBuffer(resource.getAbsolutePath());
-		Map<String, MethodParameter> queryParameters = getQueryParameters();
-		if (!queryParameters.isEmpty()) {
-			strbuf.append("?");
-			boolean first = true;
-			for (String name : queryParameters.keySet()) {
-				if (!first)
-					strbuf.append("&");
-				strbuf.append(name);
-				first = false;
-			}
-		}
-		return strbuf.toString();
-	}
+  public List<String> getConsumes() {
+    if (consumesAnnotation == null)
+      return Collections.emptyList();
+    List<String> consumedMIMEs = new ArrayList<String>();
+    for (String mime : Utils.getAnnotationValues(consumesAnnotation)) {
+      consumedMIMEs.add(mime);
+    }
+    return consumedMIMEs;
+  }
+
+  public MethodDoc getJavaDoc() {
+    return declaringMethod;
+  }
+
+  public String getDoc() {
+    return declaringMethod.commentText();
+  }
+
+  public Tag[] getDocFirstSentence() {
+    return declaringMethod.firstSentenceTags();
+  }
+
+  public Map<String, MethodParameter> getQueryParameters() {
+    return queryParameters;
+  }
+
+  public Map<String, MethodParameter> getPathParameters() {
+    return pathParameters;
+  }
+
+  public Map<String, MethodParameter> getMatrixParameters() {
+    return matrixParameters;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public MethodParameter getInputParameter() {
+    return inputParameter;
+  }
+
+  public boolean isGET() {
+    for (AnnotationDesc method : methods) {
+      if (method.annotationType().name().equals("GET"))
+        return true;
+    }
+    return false;
+  }
+
+  public String getURL(Resource resource) {
+    StringBuffer strbuf = new StringBuffer(resource.getAbsolutePath());
+    Map<String, MethodParameter> queryParameters = getQueryParameters();
+    if (!queryParameters.isEmpty()) {
+      strbuf.append("?");
+      boolean first = true;
+      for (String name : queryParameters.keySet()) {
+        if (!first)
+          strbuf.append("&");
+        strbuf.append(name);
+        first = false;
+      }
+    }
+    return strbuf.toString();
+  }
 
 }
