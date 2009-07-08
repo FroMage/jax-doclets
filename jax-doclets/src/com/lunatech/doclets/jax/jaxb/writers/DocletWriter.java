24541e48e1790f7c2a33e772bd6a393fcde393d8
stephane.epardaud <stephane.epardaud@b858e934-6b8f-11de-a2f9-0f955267958a>
2009-07-08 08:47:35 +0000

    Added LGPL notices

--- jax-doclets/src/com/lunatech/doclets/jax/jaxb/writers/DocletWriter.java
+++ jax-doclets/src/com/lunatech/doclets/jax/jaxb/writers/DocletWriter.java
@@ -1,3 +1,21 @@
+/*
+    Copyright 2009 Lunatech Research
+    
+    This file is part of jax-doclets.
+
+    jax-doclets is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jax-doclets is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with jax-doclets.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lunatech.doclets.jax.jaxb.writers;
 
 import com.lunatech.doclets.jax.jaxb.model.JAXBClass;
@@ -5,16 +23,16 @@ import com.sun.tools.doclets.formats.html.HtmlDocletWriter;
 import com.sun.tools.doclets.internal.toolkit.Configuration;
 
 public class DocletWriter extends com.lunatech.doclets.jax.writers.DocletWriter {
-	protected JAXBClass jaxbClass;
 
-	public DocletWriter(Configuration configuration, HtmlDocletWriter writer,
-			JAXBClass jaxbClass) {
-		super(configuration, writer);
-		this.jaxbClass = jaxbClass;
-	}
+  protected JAXBClass jaxbClass;
 
-	public JAXBClass getJAXBClass() {
-		return jaxbClass;
-	}
+  public DocletWriter(Configuration configuration, HtmlDocletWriter writer, JAXBClass jaxbClass) {
+    super(configuration, writer);
+    this.jaxbClass = jaxbClass;
+  }
+
+  public JAXBClass getJAXBClass() {
+    return jaxbClass;
+  }
 
 }
