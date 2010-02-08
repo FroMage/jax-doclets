ecaf77cac720be6a327a2d01026ffdd1f8d94ae7
stephane.epardaud <stephane.epardaud@b858e934-6b8f-11de-a2f9-0f955267958a>
2010-02-08 16:49:08 +0000

    New test data for demo
    git-svn-id: https://jax-doclets.googlecode.com/svn/trunk@68 b858e934-6b8f-11de-a2f9-0f955267958a

---
+++ jax-doclets/src-test/com/lunatech/doclets/jax/test/demo/doc/PizzaDAO.java
@@ -0,0 +1,19 @@
+package com.lunatech.doclets.jax.test.demo.doc;
+
+import java.util.List;
+
+public class PizzaDAO {
+
+  public List<Pizza> getPizzas(String style, int size, String query) {
+    return null;
+  }
+
+  public Pizza makePizza(List<Ingredient> ingredients) {
+    return null;
+  }
+
+  public Pizza getPizza(String name) {
+    return null;
+  }
+
+}
