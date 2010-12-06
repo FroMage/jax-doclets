19dedbb02a4359adb703bbee7f7a954eb0ee2661
stephane.epardaud <stephane.epardaud@b858e934-6b8f-11de-a2f9-0f955267958a>
2010-12-06 15:33:43 +0000

    Issue #26: Path Regex Support
    I've fixed the path characters (ironically they worked on Linux) and added the regex
    to the Path docs at the top of each resource.
    
    
    git-svn-id: https://jax-doclets.googlecode.com/svn/trunk@116 b858e934-6b8f-11de-a2f9-0f955267958a

---
+++ doclets/src/test/java/com/lunatech/doclets/jax/test/RegexPathTest.java
@@ -0,0 +1,15 @@
+package com.lunatech.doclets.jax.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("{foo:.*}-{bar:a{2}}")
+public class RegexPathTest {
+    @GET
+    @Path("{more:a{2}b{3,2}}{again}")
+    public String get(@PathParam("foo") String foo, @PathParam("bar") String bar,
+                      @PathParam("more") String more, @PathParam("again") String again){
+        return "foo";
+    }
+}
