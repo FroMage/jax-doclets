4f5353156c2fc97056f94b2ca376bc4e37196cfc
ndebeiss <ndebeiss@b858e934-6b8f-11de-a2f9-0f955267958a>
2011-08-08 14:26:19 +0000

    Improves isCollection() algorithm Fixes issue 39
    git-svn-id: https://jax-doclets.googlecode.com/svn/trunk@141 b858e934-6b8f-11de-a2f9-0f955267958a

--- doclets/src/test/java/com/lunatech/doclets/jax/jaxb/JAXBDocletTest.java
+++ doclets/src/test/java/com/lunatech/doclets/jax/jaxb/JAXBDocletTest.java
@@ -20,61 +20,64 @@ import com.sun.tools.javadoc.RootDocImpl;
 
 public class JAXBDocletTest {
 
-	private static final Logger logger = Logger.getLogger(JAXBDocletTest.class.getName());
+  private static final Logger logger = Logger.getLogger(JAXBDocletTest.class
+      .getName());
 
-	@Test
-	public void testStart() throws IOException {
-		Context context = new Context();
-		Options compOpts = Options.instance(context);
-		compOpts.put("-sourcepath", "src/test/java");
-		new PublicMessager(
-				context,
-				"test",
-				new PrintWriter(new LogWriter(Level.SEVERE), true),
-				new PrintWriter(new LogWriter(Level.WARNING), true),
-				new PrintWriter(new LogWriter(Level.FINE), true)
-		);
+  @Test
+  public void testStart() throws IOException {
+    Context context = new Context();
+    Options compOpts = Options.instance(context);
+    compOpts.put("-sourcepath", "src/test/java");
+    new PublicMessager(context, "test", new PrintWriter(new LogWriter(
+        Level.SEVERE), true), new PrintWriter(new LogWriter(Level.WARNING),
+        true), new PrintWriter(new LogWriter(Level.FINE), true));
 
-		JavadocTool javadocTool = JavadocTool.make0(context);
-		ListBuffer<String> javaNames = new ListBuffer<String>();
-		javaNames.append(ClassExample.class.getPackage().getName());
-		ListBuffer<String[]> options = new ListBuffer<String[]>();
-		ListBuffer<String> packageNames = new ListBuffer<String>();
-		ListBuffer<String> excludedPackages = new ListBuffer<String>();
+    JavadocTool javadocTool = JavadocTool.make0(context);
+    ListBuffer<String> javaNames = new ListBuffer<String>();
+    javaNames.append(ClassExample.class.getPackage().getName());
+    ListBuffer<String[]> options = new ListBuffer<String[]>();
+    ListBuffer<String> packageNames = new ListBuffer<String>();
+    ListBuffer<String> excludedPackages = new ListBuffer<String>();
 
+    RootDocImpl rootDocImpl = javadocTool.getRootDocImpl("en", "",
+        new ModifierFilter(ModifierFilter.ALL_ACCESS), javaNames.toList(),
+        options.toList(), false, packageNames.toList(),
+        excludedPackages.toList(), false, false, false);
+    logger.info(rootDocImpl.getRawCommentText());
+    JAXBDoclet.start(rootDocImpl);
+  }
 
-		RootDocImpl rootDocImpl = javadocTool.getRootDocImpl("en", "", new ModifierFilter(ModifierFilter.ALL_ACCESS), javaNames.toList(), options.toList(), false, packageNames.toList(), excludedPackages.toList(), false, false, false);
-		logger.info(rootDocImpl.getRawCommentText());
-		JAXBDoclet.start(rootDocImpl);
-	}
+  protected class LogWriter extends Writer {
 
-    protected class LogWriter extends Writer {
+    Level level;
 
-        Level level;
-
-        public LogWriter(Level level) {
-            this.level = level;
-        }
-
-        public void write(char[] chars, int offset, int length) throws IOException {
-            String s = new String(Arrays.copyOf(chars, length));
-            if (!s.equals("\n"))
-            	logger.log(level, s);
-        }
-
-        public void flush() throws IOException {}
-        public void close() throws IOException {}
+    public LogWriter(Level level) {
+      this.level = level;
     }
 
-	public class PublicMessager extends Messager {
+    public void write(char[] chars, int offset, int length) throws IOException {
+      String s = new String(Arrays.copyOf(chars, length));
+      if (!s.equals("\n"))
+        logger.log(level, s);
+    }
 
-	    public PublicMessager(Context context, String s) {
-	        super(context, s);
-	    }
+    public void flush() throws IOException {
+    }
 
-	    public PublicMessager(Context context, String s, PrintWriter printWriter, PrintWriter printWriter1, PrintWriter printWriter2) {
-	        super(context, s, printWriter, printWriter1, printWriter2);
-	    }
-	}
+    public void close() throws IOException {
+    }
+  }
+
+  public class PublicMessager extends Messager {
+
+    public PublicMessager(Context context, String s) {
+      super(context, s);
+    }
+
+    public PublicMessager(Context context, String s, PrintWriter printWriter,
+        PrintWriter printWriter1, PrintWriter printWriter2) {
+      super(context, s, printWriter, printWriter1, printWriter2);
+    }
+  }
 
 }
