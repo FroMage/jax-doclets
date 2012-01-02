2a18542c7c553b9463b9a52881441eff8fcacfc7
Stephane Epardaud <stef@epardaud.fr>
2012-01-02 14:09:32 +0100

    JPA test: added javadoc to Order

--- doclets/src/test/java/com/lunatech/doclets/jax/test/jpa/Order.java
+++ doclets/src/test/java/com/lunatech/doclets/jax/test/jpa/Order.java
@@ -15,7 +15,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
 /**
- * This is the order type, bla bla
+ * This is the order type.
  * 
  * @author stephane
  */
@@ -29,29 +29,56 @@ public class Order {
   @GeneratedValue
   public Long id;
 
+  /**
+   * Column with an explicit name
+   */
   @Column(name = "column_with_name")
   public String columnWithName;
 
+  /**
+   * Column with an annotation
+   */
   @Column
   public String columnWithAnnotation;
 
+  /**
+   * Column with no annotation
+   */
   public String columnWithoutAnnotation;
 
+  /**
+   * Transient property
+   */
   public transient String transientColumn;
 
+  /**
+   * Transient property
+   */
   @Transient
   public String transientAnnotatedColumn;
 
+  /**
+   * A Bill
+   */
   @JoinColumn(name = "bill_id")
   @OneToOne
   public Bill bill;
 
+  /**
+   * A list of Orderlines
+   */
   @OneToMany(mappedBy = "order")
   public List<Orderline> orderlineList;
 
+  /**
+   * A set of Orderlines
+   */
   @OneToMany(mappedBy = "order")
   public Set<Orderline> orderlineSet;
 
+  /**
+   * A set of Items, via a link table
+   */
   @JoinTable(name = "order2item", joinColumns = @JoinColumn(name = "item_id"), inverseJoinColumns = @JoinColumn(name = "order_id"))
   @ManyToMany
   public Set<Item> itemSet;
