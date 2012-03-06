50fcf3a6b21e8bb843c8c1f4dcdf914103c7490b
timw <timw@apache.org>
2012-03-07 11:04:50 +1300

    Handle the Class style resource locator factory methods, as long as they have generic type information.

--- doclets/src/main/java/com/lunatech/doclets/jax/jaxrs/model/ResourceMethod.java
+++ doclets/src/main/java/com/lunatech/doclets/jax/jaxrs/model/ResourceMethod.java
@@ -47,6 +47,8 @@ import com.sun.javadoc.FieldDoc;
 import com.sun.javadoc.MethodDoc;
 import com.sun.javadoc.Parameter;
 import com.sun.javadoc.Tag;
+import com.sun.javadoc.Type;
+import com.sun.javadoc.ParameterizedType;
 
 public class ResourceMethod implements Comparable<ResourceMethod> {
 
@@ -106,9 +108,17 @@ public class ResourceMethod implements Comparable<ResourceMethod> {
     setupMethods();
     setupMIMEs();
     // is this a resource locator?
-    if (methods.isEmpty() && !declaringMethod.returnType().isPrimitive())
-      resourceLocator = new ResourceClass(declaringMethod.returnType().asClassDoc(), this);
-
+    if (methods.isEmpty() && !declaringMethod.returnType().isPrimitive()) {
+      // Handle Class style resource locator factory methods
+      Type t = declaringMethod.returnType();
+    	if("java.lang.Class".equals(t.qualifiedTypeName())) {
+        ParameterizedType p = t.asParameterizedType();
+       	 	if (p != null) {
+       	 		t = p.typeArguments()[0];
+       	 	}
+    	}
+       resourceLocator = new ResourceClass(t.asClassDoc(), this);
+    }
   }
 
   public ResourceClass getResourceClass() {
