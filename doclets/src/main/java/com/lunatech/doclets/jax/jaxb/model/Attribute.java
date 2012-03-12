1829701e2c6892f3aadc74009510e4f91bb643e3
Martin Ždila <m.zdila@gmail.com>
2012-03-12 13:42:01 +0100

    column Required also for attributes

--- doclets/src/main/java/com/lunatech/doclets/jax/jaxb/model/Attribute.java
+++ doclets/src/main/java/com/lunatech/doclets/jax/jaxb/model/Attribute.java
@@ -21,10 +21,10 @@ package com.lunatech.doclets.jax.jaxb.model;
 import com.sun.javadoc.AnnotationDesc;
 import com.sun.javadoc.ProgramElementDoc;
 
-public class Attribute extends JAXBMember {
+public class Attribute extends Node {
 
   public Attribute(JAXBClass klass, ProgramElementDoc property, String name, AnnotationDesc xmlAttributeAnnotation) {
     super(klass, property, name, xmlAttributeAnnotation);
   }
-
+  
 }
